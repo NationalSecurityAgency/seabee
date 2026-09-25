86dda39cae3f0f12ca60e1e851dd901167535dcd
Alan Wandke <adwandk@uwe.nsa.gov>
2026-09-25 16:06:36 -0400

    fix: make dnf remove non-interactive
    Signed-off-by: Alan Wandke <adwandk@uwe.nsa.gov>

--- scripts/update_root_dependencies.sh
+++ scripts/update_root_dependencies.sh
@@ -138,7 +138,7 @@ docker_check() {
       for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt remove $pkg; done
       docker_install
     elif [ $USE_DNF -eq 1 ]; then
-      dnf remove docker \
+      dnf remove -y docker \
         docker-client \
         docker-client-latest \
         docker-common \
